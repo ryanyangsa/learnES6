f5ee08b42db525410be0addf1ef7d0586b2de390
Ryan Yang <ryan.yang.sa@gmail.com>
2018-07-08 12:19:57 +0900

    ESlint 권고사항 반영

--- Leçon_02/public/es6/test.js
+++ Leçon_02/public/es6/test.js
@@ -2,17 +2,17 @@
 
 // ES6 기능: 블록 스코프 변수 선언
 const sentences = [
-  { subject: 'JavaScript', verb: 'is', object: 'great' },
-  { subject: 'Elephants', verb: 'are', object: 'large' },
+    { subject: 'JavaScript', verb: 'is', object: 'great' },
+    { subject: 'Elephants', verb: 'are', object: 'large' },
 ];
 
 // ES6 기능: 객체 분해
 function say({ subject, verb, object }) {
-  // ES6 기능: 템플릿 문자열
-  console.log(`${subject} ${verb} ${object}`);
+    // ES6 기능: 템플릿 문자열
+    console.log(`${subject} ${verb} ${object}`);
 }
 
 // ES6 기능: for..of
 for(let s of sentences) {
-  say(s);
+    say(s);
 }

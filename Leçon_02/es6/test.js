4dcc43e4a563444e3307d76d93d595130104550a
Ryan Yang <ryan.yang.sa@gmail.com>
2018-07-08 11:56:35 +0900

    gulp 실습

---
+++ Leçon_02/es6/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// ES6 기능: 블록 스코프 변수 선언
+const sentences = [
+  { subject: 'JavaScript', verb: 'is', object: 'great' },
+  { subject: 'Elephants', verb: 'are', object: 'large' },
+];
+
+// ES6 기능: 객체 분해
+function say({ subject, verb, object }) {
+  // ES6 기능: 템플릿 문자열
+  console.log(`${subject} ${verb} ${object}`);
+}
+
+// ES6 기능: for..of
+for(let s of sentences) {
+  say(s);
+}

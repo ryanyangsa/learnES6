6d7e4c79a6816e96d049448b92c82a617ed60ab9
Ryan Yang <ryan.yang.sa@gmail.com>
2018-07-08 12:19:23 +0900

    ESlint 설정 추가

--- Leçon_02/gulpfile.js
+++ Leçon_02/gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
+  // ESlint 실행
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+
   // 노드 소스
   gulp.src("es6/**/*.js")
     .pipe(babel())
